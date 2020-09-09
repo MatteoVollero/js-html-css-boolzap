3823045f726eaa457f52c6e1c806959a39b9b17d
MatteoVollero <m.vollero120796@gmail.com>
2020-09-09 20:00:57 +0200

    Aggiustare messaggi

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
-
+// Ripartire da qui e capire perche ti stampa due volte le cose
   $(".fa-telegram-plane").click(function(){
     var testo = $("#messaggio").val();
-    if(testo != ""){
-      var messaggio = $(".messages").clone();
+      if(testo != ""){
+       console.log(testo);
+       var messaggio = $(".message-row").clone();
+       console.log("[DEBUG] ----> messaggio = " + messaggio);
+       messaggio.find(".message-text").text(testo);
+       messaggio.find(".message-time").text("12:00");
+       messaggio.addClass("destra");
 
-      messaggio.find(".message-text").text(testo);
-      messaggio.find(".message-time").text("11:05");
-      messaggio.addClass("owner-message");
-
-      $(".messages").append(messaggio);
-
-    }
-
-    $("#messaggio").val() = "";
+       $(".chat-window").append(messaggio);
+       $("#messaggio").val("");
+      }
   });
 
 });
